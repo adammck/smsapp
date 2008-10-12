5e5d4364f341e06245b1c2e9e4656e5662adbe19
Adam Mckaig <adam.mckaig@gmail.com>
2008-10-12 14:49:44 -0400

    added CallerError exception, to abort incomming sms handlers cleanly

--- smsapp.py
+++ smsapp.py
@@ -4,16 +4,22 @@
 import re
 
 
+class CallerError(Exception):
+	"""Raised during incomming SMS processing, to inform the
+	   caller that they did something wrong, and abort the action"""
+	pass
+
+
 class SmsApplication():
 	LOG_PREFIX = {
-		"info":  "   ",
+		"info": "\x1b[40m   \x1b[0m",
 		"warn": "\x1b[41mERR\x1b[0m",
 		"out":  "\x1b[45m >>\x1b[0m",
 		"in":   "\x1b[46m<< \x1b[0m" }
 		
 	def log(self, msg, type="info"):
 		print self.LOG_PREFIX[type], msg
-
+	
 	def __init__(self, backend, sender_args=[], receiver_args=[]):
 		self.receiver = backend.SmsReceiver(self._incoming_sms, *receiver_args)
 		self.sender = backend.SmsSender(*sender_args)
@@ -34,25 +40,37 @@ class SmsApplication():
 	def flush(self):
 		self.sender.flush()
 	
+	
 	def _incoming_sms(self, caller, msg):
 		self.log("%s: %r" % (caller, msg), "in")
 		
 		try:
+			# if we are using magic keywords,
+			# then attempt to find a match
 			if hasattr(self, "kw"):
-				func, captures = self.kw.match(self, msg)
-				func(self, caller, *captures)
-				return
+				try: func, captures = self.kw.match(self, msg)
+				except ValueError: pass
 				
-		except ValueError:
-			self.log("No matching method", "warn")
+				else:
+					# invoke the matching method
+					# and stop further processing
+					func(self, caller, *captures)
+					return
 		
-		# the application isn't using sms keyword decorators,
-		# or nothing matched. either way, dispatch to the
-		# "incomming_sms" method, which should be overloaded
-		self.incoming_sms(caller, msg)
+			# the application isn't using sms keyword decorators,
+			# or nothing matched. either way, dispatch to the
+			# "incomming_sms" method, which should be overloaded
+			self.incoming_sms(caller, msg)
+			
+			
+		# the request could not be completed
+		# because THE USER did something wrong
+		except CallerError, ex:
+			self.send(caller, ex.args)
+	
 	
 	def incoming_sms(self, caller, msg):
-		pass
+		self.log("Incomming message ignored", "warn")
 	
 	def run(self):
 		app = self.__class__.__name__
